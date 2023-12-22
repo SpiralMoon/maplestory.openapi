2946e0d13053b328281e7b3c68031060f1b0e41b
SpiralMoon <dev.spiralmoon@gmail.com>
2023-12-22 22:24:02 +0900

    Create js union api DTO classes

---
+++ js/src/maplestory/api/response/unionRaiderDtoBody.ts
@@ -0,0 +1,38 @@
+type UnionRaiderDtoBody = {
+    date: string;
+    union_raider_stat: string[];
+    union_occupied_stat: string[];
+    union_inner_stat: UnionRaiderInnerStatDtoBody[];
+    union_block: UnionRaiderBlockDtoBody[];
+};
+
+type UnionRaiderInnerStatDtoBody = {
+    stat_field_id: string;
+    stat_field_effect: string;
+};
+
+type UnionRaiderBlockDtoBody = {
+    block_type: string;
+    block_class: string;
+    block_level: string;
+    block_control_point: UnionRaiderBlockControlPointDtoBody;
+    block_position: UnionRaiderBlockPositionDtoBody[];
+};
+
+type UnionRaiderBlockControlPointDtoBody = {
+    x: number;
+    y: number;
+}
+
+type UnionRaiderBlockPositionDtoBody = {
+    x: number;
+    y: number;
+}
+
+export type {
+    UnionRaiderDtoBody,
+    UnionRaiderInnerStatDtoBody,
+    UnionRaiderBlockDtoBody,
+    UnionRaiderBlockControlPointDtoBody,
+    UnionRaiderBlockPositionDtoBody
+};
