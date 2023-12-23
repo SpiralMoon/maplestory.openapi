92e1ee4d5a46e9280c6e684a2c94ade385c2d022
SpiralMoon <dev.spiralmoon@gmail.com>
2023-12-23 13:29:22 +0900

    Create java union api DTO classes

---
+++ java/src/main/java/dev/spiralmoon/maplestory/api/dto/UnionRaiderBlockPositionDTO.java
@@ -0,0 +1,27 @@
+package dev.spiralmoon.maplestory.api.dto;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * 유니온 블록이 차지하고 있는 영역 좌표들
+ */
+@AllArgsConstructor
+@Data
+@ToString
+public class UnionRaiderBlockPositionDTO {
+
+    /**
+     * 블록 X좌표
+     */
+    @SerializedName("x")
+    private long x;
+
+    /**
+     * 블록 Y좌표
+     */
+    @SerializedName("y")
+    private long y;
+}
