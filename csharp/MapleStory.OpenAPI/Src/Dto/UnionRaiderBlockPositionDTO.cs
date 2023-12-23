253ee9cea1554eea4efe33e003ed7719e8f355cd
SpiralMoon <dev.spiralmoon@gmail.com>
2023-12-23 22:45:11 +0900

    Create c# character api DTO classes

---
+++ csharp/MapleStory.OpenAPI/Src/Dto/UnionRaiderBlockPositionDTO.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace MapleStory.OpenAPI.Dto
+{
+    /// <summary>
+    /// 유니온 블록이 차지하고 있는 영역 좌표들
+    /// </summary>
+    public class UnionRaiderBlockPositionDTO
+    {
+        /// <summary>
+        /// 블록 X좌표
+        /// </summary>
+        [JsonProperty("x")]
+        public long X { get; set; }
+
+        /// <summary>
+        /// 블록 Y좌표
+        /// </summary>
+        [JsonProperty("y")]
+        public long Y { get; set; }
+    }
+}
