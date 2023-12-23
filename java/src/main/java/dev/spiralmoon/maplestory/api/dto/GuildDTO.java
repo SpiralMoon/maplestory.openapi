f4ad52500a04e40a094cde3899ad759851bff1fa
SpiralMoon <dev.spiralmoon@gmail.com>
2023-12-23 13:43:48 +0900

    Create java guild api DTO classes

---
+++ java/src/main/java/dev/spiralmoon/maplestory/api/dto/GuildDTO.java
@@ -0,0 +1,21 @@
+package dev.spiralmoon.maplestory.api.dto;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * 길드 식별자 정보
+ */
+@AllArgsConstructor
+@Data
+@ToString
+public class GuildDTO {
+
+    /**
+     * 길드 식별자
+     */
+    @SerializedName("oguild_id")
+    private String oguildId;
+}
---
+++ java/src/main/java/dev/spiralmoon/maplestory/api/dto/GuildSkillDTO.java
@@ -0,0 +1,45 @@
+package dev.spiralmoon.maplestory.api.dto;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * 길드 스킬 정보
+ */
+@AllArgsConstructor
+@Data
+@ToString
+public class GuildSkillDTO {
+
+    /**
+     * 스킬 명
+     */
+    @SerializedName("skill_name")
+    private String skillName;
+
+    /**
+     * 스킬 설명
+     */
+    @SerializedName("skill_description")
+    private String skillDescription;
+
+    /**
+     * 스킬 레벨
+     */
+    @SerializedName("skill_level")
+    private long skillLevel;
+
+    /**
+     * 스킬 레벨 별 효과
+     */
+    @SerializedName("skill_effect")
+    private String skillEffect;
+
+    /**
+     * 스킬 아이콘
+     */
+    @SerializedName("skill_icon")
+    private String skillIcon;
+}
