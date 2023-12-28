4fbdfbe78a499037690281cabbd2654ad1961e48
Taiki Ikeda <so99ynoodles@gmail.com>
2023-12-28 12:35:17 +0900

    Add get_character_vmatrix api

--- python/maplestory_openapi/api/maplestory_api.py
+++ python/maplestory_openapi/api/maplestory_api.py
@@ -19,8 +19,8 @@ from maplestory_openapi.api.dto.character_android_equipment import CharacterAndr
 from maplestory_openapi.api.dto.character_pet_equipment import CharacterPetEquipment
 from maplestory_openapi.api.dto.character_skill import CharacterSkill
 from maplestory_openapi.api.dto.character_link_skill import CharacterLinkSkill
+from maplestory_openapi.api.dto.character_vmatrix import CharacterVMatrix
 from maplestory_openapi.api.maplestory_api_error import MapleStoryApiError, MapleStoryApiException
-
 from maplestory_openapi.api.utils.date import get_proper_default_datetime
 
 
@@ -345,6 +345,24 @@ class MapleStoryApi(BaseModel):
         }
         r = self.fetch(path, query)
         return CharacterLinkSkill(**r)
+
+    def get_character_vmatrix(self, ocid: str, date: datetime = get_proper_default_datetime()) -> CharacterVMatrix:
+        """V매트릭스 슬롯 정보와 장착한 V코어 정보를 조회합니다.
+
+        - 2023년 12월 21일 데이터부터 조회할 수 있습니다.
+        - 오전 0시부터 전일 데이터 조회가 가능합니다.
+        - 게임 콘텐츠 변경으로 ocid가 변경될 수 있습니다. ocid 기반 서비스 갱신시 유의해 주시기 바랍니다.
+
+        @param ocid(str): 캐릭터 식별자(ocid)
+        @param date(datetime): 조회 기준일(KST)
+        """
+        path = 'maplestory/v1/character/vmatrix'
+        query = {
+            'ocid': ocid,
+            'date': self.to_date_string(datetime(2023, 12, 21), date),
+        }
+        r = self.fetch(path, query)
+        return CharacterVMatrix(**r)
 
     def fetch(self, path: str, query: dict) -> Any:
         r = requests.get(
