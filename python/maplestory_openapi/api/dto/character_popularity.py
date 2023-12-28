d9158474aad5418bf4599b3cd4e6d9b8ee192f19
Taiki Ikeda <so99ynoodles@gmail.com>
2023-12-28 10:15:23 +0900

    Add comments for each dto

--- python/maplestory_openapi/api/dto/character_popularity.py
+++ python/maplestory_openapi/api/dto/character_popularity.py
@@ -3,5 +3,10 @@ from pydantic import BaseModel
 
 
 class CharacterPopularity(BaseModel):
+    """캐릭터 인기도 정보
+
+    date(datetime): 조회 기준일
+    popularity(int): 인기도
+    """
     date: datetime
     popularity: int
