b2982573e6445bbfae907ce9e198ada2ee6a7c62
SpiralMoon <dev.spiralmoon@gmail.com>
2024-01-01 19:58:57 +0900

    Fix java 'yyyy-MM-dd' parsing bug

--- java/src/main/java/dev/spiralmoon/maplestory/api/Utils.java
+++ java/src/main/java/dev/spiralmoon/maplestory/api/Utils.java
@@ -2,21 +2,42 @@ package dev.spiralmoon.maplestory.api;
 
 import lombok.NonNull;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Utils {
 
     public static LocalDateTime toLocalDateTime(@NonNull String date) {
 
-        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
-        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mmXXX");
+        final String[] patterns = {
+                "yyyy-MM-dd'T'HH:mm:ss.SSSX",
+                "yyyy-MM-dd'T'HH:mmXXX",
+                "yyyy-MM-dd"
+        };
 
-        try {
-            return LocalDateTime.parse(date, formatter1);
+        LocalDateTime kstDateTime = null;
+
+        for (String pattern : patterns) {
+            try {
+
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+
+                if (pattern.equals("yyyy-MM-dd")) {
+                    LocalDate localDate = LocalDate.parse(date, formatter);
+                    kstDateTime = localDate.atStartOfDay().atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+                } else {
+                    kstDateTime = LocalDateTime.parse(date, formatter).atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+                }
+
+                break;
+            } catch (DateTimeParseException e) {
+                // nothing to do
+            }
         }
-        catch (Exception e) {
-            return LocalDateTime.parse(date, formatter2);
-        }
+
+        return kstDateTime;
     }
 }
