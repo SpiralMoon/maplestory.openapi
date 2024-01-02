9dfd0201dbc6aad6d874c80facc6d5bd5f37d5a1
SpiralMoon <dev.spiralmoon@gmail.com>
2024-01-03 01:42:23 +0900

    Update python setup.py

--- python/setup.py
+++ python/setup.py
@@ -2,12 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='maplestory-openapi',
-    version='0.0.1',
-    description='MapleStory OpenAPI',
+    version='2.3.0',
+    description='This Python library enables the use of the MapleStory OpenAPI of Nexon.',
     author='so99ynoodles',
     url='https://github.com/SpiralMoon/maplestory.openapi',
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
+    install_requires=[
+        'pydantic <= 2.5.0',
+        'requests <= 2.31.0'
+    ],
     packages=find_packages(),
     license='MIT',
     python_requires='>=3.9',
