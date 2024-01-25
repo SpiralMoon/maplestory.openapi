d6965cbc23f630969e4e617e3cb8cca353a2dd16
SpiralMoon <dev.spiralmoon@gmail.com>
2024-01-26 07:21:11 +0900

    Add js ability preset properties to CharacterAbilityDTO

--- js/src/maplestory/api/dto/character/characterAbilityDto.ts
+++ js/src/maplestory/api/dto/character/characterAbilityDto.ts
@@ -1,4 +1,5 @@
 import { CharacterAbilityInfoDto } from './characterAbilityInfoDto';
+import { CharacterAbilityPresetDto } from './characterAbilityPresetDto';
 import { CharacterAbilityDtoBody } from '../../response/character/characterAbilityDtoBody';
 
 /**
@@ -25,8 +26,37 @@ class CharacterAbilityDto {
    */
   remainFame: number;
 
+  /**
+   * 적용 중인 어빌리티 프리셋 번호
+   */
+  presetNo: number | null;
+
+  /**
+   * 어빌리티 1번 프리셋 전체 정보
+   */
+  abilityPreset1: CharacterAbilityPresetDto | null;
+
+  /**
+   * 어빌리티 2번 프리셋 전체 정보
+   */
+  abilityPreset2: CharacterAbilityPresetDto | null;
+
+  /**
+   * 어빌리티 3번 프리셋 전체 정보
+   */
+  abilityPreset3: CharacterAbilityPresetDto | null;
+
   constructor(obj: CharacterAbilityDtoBody) {
-    const { date, ability_grade, ability_info, remain_fame } = obj;
+    const {
+      date,
+      ability_grade,
+      ability_info,
+      remain_fame,
+      preset_no,
+      ability_preset_1,
+      ability_preset_2,
+      ability_preset_3,
+    } = obj;
 
     // 날짜는 Date 객체로 변환
     this.date = new Date(date);
@@ -35,6 +65,16 @@ class CharacterAbilityDto {
       (info) => new CharacterAbilityInfoDto(info),
     );
     this.remainFame = remain_fame;
+    this.presetNo = preset_no;
+    this.abilityPreset1 = ability_preset_1
+      ? new CharacterAbilityPresetDto(ability_preset_1)
+      : null;
+    this.abilityPreset2 = ability_preset_2
+      ? new CharacterAbilityPresetDto(ability_preset_2)
+      : null;
+    this.abilityPreset3 = ability_preset_3
+      ? new CharacterAbilityPresetDto(ability_preset_3)
+      : null;
   }
 }
 
--- js/src/maplestory/api/dto/character/characterAbilityInfoDto.ts
+++ js/src/maplestory/api/dto/character/characterAbilityInfoDto.ts
@@ -4,8 +4,19 @@ import { CharacterAbilityInfoDtoBody } from '../../response/character/characterA
  * 캐릭터 어빌리티 상세 정보
  */
 class CharacterAbilityInfoDto {
+  /**
+   * 어빌리티 번호
+   */
   abilityNo: string;
+
+  /**
+   * 어빌리티 등급
+   */
   abilityGrade: string;
+
+  /**
+   * 어빌리티 옵션 및 수치
+   */
   abilityValue: string;
 
   constructor(obj: CharacterAbilityInfoDtoBody) {
