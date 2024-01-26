9022f8d3b54b7b76a2f2fbeb2b9e6b1525ef20f2
SpiralMoon <dev.spiralmoon@gmail.com>
2024-01-26 19:00:02 +0900

    Create c# potential api DTO classes

---
+++ csharp/MapleStory.OpenAPI/Src/Dto/History/PotentialResultOptionDTO.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace MapleStory.OpenAPI.Dto
+{
+    /// <summary>
+    /// 잠재능력 재설정 결과 옵션 정보
+    /// </summary>
+    public class PotentialResultOptionDTO
+    {
+        /// <summary>
+        /// 옵션 명
+        /// </summary>
+        [JsonProperty("value")]
+        public string Value { get; set; }
+
+        /// <summary>
+        /// 옵션 등급
+        /// </summary>
+        [JsonProperty("grade")]
+        public string Grade { get; set; }
+
+        public PotentialOptionGrade GradeEnum
+        {
+            get {
+                return Utils.PotentialOptionGradeFromString(this.Grade);
+            }
+        }
+    }
+}
