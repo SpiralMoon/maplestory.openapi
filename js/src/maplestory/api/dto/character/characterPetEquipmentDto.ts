d5c0f3eee41ff3efa4745674b7d9f268eb17b554
SpiralMoon <dev.spiralmoon@gmail.com>
2024-01-26 10:54:18 +0900

    Add js appearance properties to CharacterPetEquipmentDTO, Update nullable

--- js/src/maplestory/api/dto/character/characterPetEquipmentDto.ts
+++ js/src/maplestory/api/dto/character/characterPetEquipmentDto.ts
@@ -14,32 +14,32 @@ class CharacterPetEquipmentDto {
   /**
    * 펫1 명
    */
-  pet1Name: string;
+  pet1Name: string | null;
 
   /**
    * 펫1 닉네임
    */
-  pet1Nickname: string;
+  pet1Nickname: string | null;
 
   /**
    * 펫1 아이콘
    */
-  pet1Icon: string;
+  pet1Icon: string | null;
 
   /**
    * 펫1 설명
    */
-  pet1Description: string;
+  pet1Description: string | null;
 
   /**
    * 펫1 장착 정보
    */
-  pet1Equipment: CharacterPetEquipmentItemDto;
+  pet1Equipment: CharacterPetEquipmentItemDto | null;
 
   /**
    * 펫1 펫 버프 자동스킬 정보
    */
-  pet1AutoSkill: CharacterPetEquipmentAutoSkillDto;
+  pet1AutoSkill: CharacterPetEquipmentAutoSkillDto | null;
 
   /**
    * 펫1 원더 펫 종류
@@ -54,37 +54,47 @@ class CharacterPetEquipmentDto {
   /**
    * 펫1 마법의 시간 (KST, 시간 단위 데이터로 분은 일괄 0으로 표기)
    */
-  pet1DateExpire: Date;
+  pet1DateExpire: Date | null;
+
+  /**
+   * 펫1 외형
+   */
+  pet1Appearance: string | null;
+
+  /**
+   * 펫1 외형 아이콘
+   */
+  pet1AppearanceIcon: string | null;
 
   /**
    * 펫2 명
    */
-  pet2Name: string;
+  pet2Name: string | null;
 
   /**
    * 펫2 닉네임
    */
-  pet2Nickname: string;
+  pet2Nickname: string | null;
 
   /**
    * 펫2 아이콘
    */
-  pet2Icon: string;
+  pet2Icon: string | null;
 
   /**
    * 펫2 설명
    */
-  pet2Description: string;
+  pet2Description: string | null;
 
   /**
    * 펫2 장착 정보
    */
-  pet2Equipment: CharacterPetEquipmentItemDto;
+  pet2Equipment: CharacterPetEquipmentItemDto | null;
 
   /**
    * 펫2 펫 버프 자동스킬 정보
    */
-  pet2AutoSkill: CharacterPetEquipmentAutoSkillDto;
+  pet2AutoSkill: CharacterPetEquipmentAutoSkillDto | null;
 
   /**
    * 펫2 원더 펫 종류
@@ -96,40 +106,50 @@ class CharacterPetEquipmentDto {
    */
   pet2Skill: string[];
 
-  /*
+  /**
    * 펫2 마법의 시간 (KST, 시간 단위 데이터로 분은 일괄 0으로 표기)
    */
-  pet2DateExpire: Date;
+  pet2DateExpire: Date | null;
+
+  /**
+   * 펫2 외형
+   */
+  pet2Appearance: string | null;
+
+  /**
+   * 펫2 외형 아이콘
+   */
+  pet2AppearanceIcon: string | null;
 
   /**
    * 펫3 명
    */
-  pet3Name: string;
+  pet3Name: string | null;
 
   /**
    * 펫3 닉네임
    */
-  pet3Nickname: string;
+  pet3Nickname: string | null;
 
   /**
    * 펫3 아이콘
    */
-  pet3Icon: string;
+  pet3Icon: string | null;
 
   /**
    * 펫3 설명
    */
-  pet3Description: string;
+  pet3Description: string | null;
 
   /**
    * 펫3 장착 정보
    */
-  pet3Equipment: CharacterPetEquipmentItemDto;
+  pet3Equipment: CharacterPetEquipmentItemDto | null;
 
   /**
    * 펫3 펫 버프 자동스킬 정보
    */
-  pet3AutoSkill: CharacterPetEquipmentAutoSkillDto;
+  pet3AutoSkill: CharacterPetEquipmentAutoSkillDto | null;
 
   /**
    * 펫3 원더 펫 종류
@@ -141,10 +161,20 @@ class CharacterPetEquipmentDto {
    */
   pet3Skill: string[];
 
-  /*
+  /**
    * 펫3 마법의 시간 (KST, 시간 단위 데이터로 분은 일괄 0으로 표기)
    */
-  pet3DateExpire: Date;
+  pet3DateExpire: Date | null;
+
+  /**
+   * 펫3 외형
+   */
+  pet3Appearance: string | null;
+
+  /**
+   * 펫3 외형 아이콘
+   */
+  pet3AppearanceIcon: string | null;
 
   constructor(obj: CharacterPetEquipmentDtoBody) {
     const {
@@ -158,6 +188,8 @@ class CharacterPetEquipmentDto {
       pet_1_pet_type,
       pet_1_skill,
       pet_1_date_expire,
+      pet_1_appearance,
+      pet_1_appearance_icon,
       pet_2_name,
       pet_2_nickname,
       pet_2_icon,
@@ -167,6 +199,8 @@ class CharacterPetEquipmentDto {
       pet_2_pet_type,
       pet_2_skill,
       pet_2_date_expire,
+      pet_2_appearance,
+      pet_2_appearance_icon,
       pet_3_name,
       pet_3_nickname,
       pet_3_icon,
@@ -176,6 +210,8 @@ class CharacterPetEquipmentDto {
       pet_3_pet_type,
       pet_3_skill,
       pet_3_date_expire,
+      pet_3_appearance,
+      pet_3_appearance_icon,
     } = obj;
 
     this.date = new Date(date);
@@ -183,35 +219,41 @@ class CharacterPetEquipmentDto {
     this.pet1Nickname = pet_1_nickname;
     this.pet1Icon = pet_1_icon;
     this.pet1Description = pet_1_description;
-    this.pet1Equipment = new CharacterPetEquipmentItemDto(pet_1_equipment);
-    this.pet1AutoSkill = new CharacterPetEquipmentAutoSkillDto(
+    this.pet1Equipment = pet_1_equipment ? new CharacterPetEquipmentItemDto(pet_1_equipment) : null;
+    this.pet1AutoSkill = pet_1_auto_skill ? new CharacterPetEquipmentAutoSkillDto(
       pet_1_auto_skill,
-    );
+    ) : null;
     this.pet1PetType = pet_1_pet_type;
     this.pet1Skill = pet_1_skill;
-    this.pet1DateExpire = new Date(pet_1_date_expire);
+    this.pet1DateExpire = pet_1_date_expire ? new Date(pet_1_date_expire) : null;
+    this.pet1Appearance = pet_1_appearance;
+    this.pet1AppearanceIcon = pet_1_appearance_icon;
     this.pet2Name = pet_2_name;
     this.pet2Nickname = pet_2_nickname;
     this.pet2Icon = pet_2_icon;
     this.pet2Description = pet_2_description;
-    this.pet2Equipment = new CharacterPetEquipmentItemDto(pet_2_equipment);
-    this.pet2AutoSkill = new CharacterPetEquipmentAutoSkillDto(
+    this.pet2Equipment = pet_2_equipment ? new CharacterPetEquipmentItemDto(pet_2_equipment) : null;
+    this.pet2AutoSkill = pet_2_auto_skill ? new CharacterPetEquipmentAutoSkillDto(
       pet_2_auto_skill,
-    );
+    ) : null;
     this.pet2PetType = pet_2_pet_type;
     this.pet2Skill = pet_2_skill;
-    this.pet2DateExpire = new Date(pet_2_date_expire);
+    this.pet2DateExpire = pet_2_date_expire ? new Date(pet_2_date_expire) : null;
+    this.pet2Appearance = pet_2_appearance;
+    this.pet2AppearanceIcon = pet_2_appearance_icon;
     this.pet3Name = pet_3_name;
     this.pet3Nickname = pet_3_nickname;
     this.pet3Icon = pet_3_icon;
     this.pet3Description = pet_3_description;
-    this.pet3Equipment = new CharacterPetEquipmentItemDto(pet_3_equipment);
-    this.pet3AutoSkill = new CharacterPetEquipmentAutoSkillDto(
+    this.pet3Equipment = pet_3_equipment ? new CharacterPetEquipmentItemDto(pet_3_equipment) : null;
+    this.pet3AutoSkill = pet_3_auto_skill ? new CharacterPetEquipmentAutoSkillDto(
       pet_3_auto_skill,
-    );
+    ) : null;
     this.pet3PetType = pet_3_pet_type;
     this.pet3Skill = pet_3_skill;
-    this.pet3DateExpire = new Date(pet_3_date_expire);
+    this.pet3DateExpire = pet_3_date_expire ? new Date(pet_3_date_expire) : null;
+    this.pet3Appearance = pet_3_appearance;
+    this.pet3AppearanceIcon = pet_3_appearance_icon;
   }
 }
 
