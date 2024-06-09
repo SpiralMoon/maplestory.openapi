4a54b70ff71c13bd954df268107a14ede12f3941
SpiralMoon <dev.spiralmoon@gmail.com>
2024-06-10 04:41:22 +0900

    Create c# character list api DTO classes

---
+++ csharp/MapleStory.OpenAPI/Src/Dto/Character/CharacterListAccountDTO.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace MapleStory.OpenAPI.Dto
+{
+    /// <summary>
+    /// 메이플스토리 계정
+    /// </summary>
+    public class CharacterListAccountDTO
+    {
+        /// <summary>
+        /// 메이플스토리 계정 식별자
+        /// </summary>
+        [JsonProperty("account_id")]
+        public string AccountId { get; set; }
+
+        /// <summary>
+        /// 캐릭터 목록
+        /// </summary>
+        [JsonProperty("character_list")]
+        public List<CharacterListAccountCharacterDTO> CharacterList { get; set; }
+    }
+}
---
+++ csharp/MapleStory.OpenAPI/Src/Dto/Character/CharacterListDTO.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace MapleStory.OpenAPI.Dto
+{
+    /// <summary>
+    /// 계정의 보유 캐릭터 목록
+    /// </summary>
+    public class CharacterListDTO
+    {
+        /// <summary>
+        /// 메이플스토리 계정 목록
+        /// </summary>
+        [JsonProperty("account_list")]
+        public List<CharacterListAccountDTO> AccountList { get; set; }
+    }
+}
