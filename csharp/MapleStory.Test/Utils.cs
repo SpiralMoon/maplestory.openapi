1a938c50701e05257e253845ab98bf7244ecd82c
SpiralMoon <dev.spiralmoon@gmail.com>
2024-06-14 01:05:35 +0900

    Create c# test scripts

---
+++ csharp/MapleStory.Test/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace MapleStory.Test
+{
+    public static class Utils
+    {
+        public static string ToJson(this object obj)
+        {
+            return JsonSerializer.Serialize(obj, new JsonSerializerOptions { WriteIndented = true });
+        }
+    }
+}
