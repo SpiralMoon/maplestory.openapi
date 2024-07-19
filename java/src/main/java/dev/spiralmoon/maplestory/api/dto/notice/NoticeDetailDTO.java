bbcf477ad6b0995ccf2f83f36fa581df1d91ecbb
SpiralMoon <dev.spiralmoon@gmail.com>
2024-07-19 16:26:27 +0900

    feat: Create java notice api DTO classes

---
+++ java/src/main/java/dev/spiralmoon/maplestory/api/dto/notice/NoticeDetailDTO.java
@@ -0,0 +1,49 @@
+package dev.spiralmoon.maplestory.api.dto.notice;
+
+import com.google.gson.annotations.SerializedName;
+import dev.spiralmoon.maplestory.api.Utils;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+/**
+ * 공지사항 상세
+ */
+@AllArgsConstructor
+@Data
+@ToString
+public class NoticeDetailDTO {
+
+    /**
+     * 공지 제목
+     */
+    @SerializedName("id")
+    private String title;
+
+    /**
+     * 공지 링크
+     */
+    @SerializedName("url")
+    private String url;
+
+    /**
+     * 공지 본문
+     */
+    @SerializedName("contents")
+    private String contents;
+
+    /**
+     * 공지 등록일
+     */
+    @SerializedName("date")
+    private String date;
+
+    /**
+     * 공지 등록일
+     */
+    public LocalDateTime getDate() {
+        return Utils.toLocalDateTime(this.date);
+    }
+}
---
+++ java/src/main/java/dev/spiralmoon/maplestory/api/dto/notice/NoticeListItemDTO.java
@@ -0,0 +1,49 @@
+package dev.spiralmoon.maplestory.api.dto.notice;
+
+import com.google.gson.annotations.SerializedName;
+import dev.spiralmoon.maplestory.api.Utils;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+/**
+ * 공지사항
+ */
+@AllArgsConstructor
+@Data
+@ToString
+public class NoticeListItemDTO {
+
+    /**
+     * 공지 제목
+     */
+    @SerializedName("id")
+    private String title;
+
+    /**
+     * 공지 링크
+     */
+    @SerializedName("url")
+    private String url;
+
+    /**
+     * 공지 식별자
+     */
+    @SerializedName("notice_id")
+    private int noticeId;
+
+    /**
+     * 공지 등록일
+     */
+    @SerializedName("date")
+    private String date;
+
+    /**
+     * 공지 등록일
+     */
+    public LocalDateTime getDate() {
+        return Utils.toLocalDateTime(this.date);
+    }
+}
