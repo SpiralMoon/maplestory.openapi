e40d90071ba31aa595300b7bbe66a0f9c49cff8f
SpiralMoon <dev.spiralmoon@gmail.com>
2025-06-11 01:34:23 +0900

    build: (js) Fix module output paths and exports map for kms/msea compatibility

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,58 +1,33 @@
+export * from './maplestory/api/common/mapleStoryApi';
 export * from './maplestory/api/common/mapleStoryApiError';
 export * from './maplestory/api/common/enum/characterImage';
 export * from './maplestory/api/common/enum/potentialOptionGrade';
 
-export * from './maplestory/api/kms/mapleStoryApi';
+export * from './maplestory/api/common/dto/character/character';
+export * from './maplestory/api/common/dto/character/characterAbility';
+export * from './maplestory/api/common/dto/character/characterAndroidEquipment';
+export * from './maplestory/api/common/dto/character/characterBasic';
+export * from './maplestory/api/common/dto/character/characterBeautyEquipment';
+export * from './maplestory/api/common/dto/character/characterCashItemEquipment';
+export * from './maplestory/api/common/dto/character/characterDojang';
+export * from './maplestory/api/common/dto/character/characterHexaMatrix';
+export * from './maplestory/api/common/dto/character/characterHexaMatrixStat';
+export * from './maplestory/api/common/dto/character/characterHyperStat';
+export * from './maplestory/api/common/dto/character/characterImage';
+export * from './maplestory/api/common/dto/character/characterItemEquipment';
+export * from './maplestory/api/common/dto/character/characterLinkSkill';
+export * from './maplestory/api/common/dto/character/characterPetEquipment';
+export * from './maplestory/api/common/dto/character/characterPopularity';
+export * from './maplestory/api/common/dto/character/characterPropensity';
+export * from './maplestory/api/common/dto/character/characterSetEffect';
+export * from './maplestory/api/common/dto/character/characterSkill';
+export * from './maplestory/api/common/dto/character/characterStat';
+export * from './maplestory/api/common/dto/character/characterSymbolEquipment';
+export * from './maplestory/api/common/dto/character/characterVMatrix';
 
-export * from './maplestory/api/kms/dto/character/character';
-export * from './maplestory/api/kms/dto/character/characterAbility';
-export * from './maplestory/api/kms/dto/character/characterAndroidEquipment';
-export * from './maplestory/api/kms/dto/character/characterBasic';
-export * from './maplestory/api/kms/dto/character/characterBeautyEquipment';
-export * from './maplestory/api/kms/dto/character/characterCashItemEquipment';
-export * from './maplestory/api/kms/dto/character/characterDojang';
-export * from './maplestory/api/kms/dto/character/characterHexaMatrix';
-export * from './maplestory/api/kms/dto/character/characterHexaMatrixStat';
-export * from './maplestory/api/kms/dto/character/characterHyperStat';
-export * from './maplestory/api/kms/dto/character/characterImage';
-export * from './maplestory/api/kms/dto/character/characterItemEquipment';
-export * from './maplestory/api/kms/dto/character/characterLinkSkill';
-export * from './maplestory/api/kms/dto/character/characterList';
-export * from './maplestory/api/kms/dto/character/characterPetEquipment';
-export * from './maplestory/api/kms/dto/character/characterPopularity';
-export * from './maplestory/api/kms/dto/character/characterPropensity';
-export * from './maplestory/api/kms/dto/character/characterSetEffect';
-export * from './maplestory/api/kms/dto/character/characterSkill';
-export * from './maplestory/api/kms/dto/character/characterStat';
-export * from './maplestory/api/kms/dto/character/characterSymbolEquipment';
-export * from './maplestory/api/kms/dto/character/characterVMatrix';
+export * from './maplestory/api/common/dto/guild/guild';
+export * from './maplestory/api/common/dto/guild/guildBasic';
 
-export * from './maplestory/api/kms/dto/guild/guild';
-export * from './maplestory/api/kms/dto/guild/guildBasic';
-
-export * from './maplestory/api/kms/dto/history/cubeHistory';
-export * from './maplestory/api/kms/dto/history/potentialHistory';
-export * from './maplestory/api/kms/dto/history/starforceHistory';
-
-export * from './maplestory/api/kms/dto/notice/cashshopNoticeDetail';
-export * from './maplestory/api/kms/dto/notice/cashshopNoticeList';
-export * from './maplestory/api/kms/dto/notice/eventNoticeDetail';
-export * from './maplestory/api/kms/dto/notice/eventNoticeList';
-export * from './maplestory/api/kms/dto/notice/noticeDetail';
-export * from './maplestory/api/kms/dto/notice/noticeList';
-export * from './maplestory/api/kms/dto/notice/updateNoticeDetail';
-export * from './maplestory/api/kms/dto/notice/updateNoticeList';
-
-export * from './maplestory/api/kms/dto/ranking/achievementRanking';
-export * from './maplestory/api/kms/dto/ranking/dojangRanking';
-export * from './maplestory/api/kms/dto/ranking/guildRanking';
-export * from './maplestory/api/kms/dto/ranking/overallRanking';
-export * from './maplestory/api/kms/dto/ranking/theSeedRanking';
-export * from './maplestory/api/kms/dto/ranking/unionRanking';
-
-export * from './maplestory/api/kms/dto/union/union';
-export * from './maplestory/api/kms/dto/union/unionArtifact';
-export * from './maplestory/api/kms/dto/union/unionChampion';
-export * from './maplestory/api/kms/dto/union/unionRaider';
-
-export * from './maplestory/api/kms/dto/inspectionInfo';
+export * from './maplestory/api/common/dto/union/union';
+export * from './maplestory/api/common/dto/union/unionArtifact';
+export * from './maplestory/api/common/dto/union/unionRaider';
---
+++ js/src/maplestory/api/kms/index.ts
@@ -0,0 +1,54 @@
+export * from './mapleStoryApi';
+
+export * from './dto/character/character';
+export * from './dto/character/characterAbility';
+export * from './dto/character/characterAndroidEquipment';
+export * from './dto/character/characterBasic';
+export * from './dto/character/characterBeautyEquipment';
+export * from './dto/character/characterCashItemEquipment';
+export * from './dto/character/characterDojang';
+export * from './dto/character/characterHexaMatrix';
+export * from './dto/character/characterHexaMatrixStat';
+export * from './dto/character/characterHyperStat';
+export * from './dto/character/characterImage';
+export * from './dto/character/characterItemEquipment';
+export * from './dto/character/characterLinkSkill';
+export * from './dto/character/characterList';
+export * from './dto/character/characterPetEquipment';
+export * from './dto/character/characterPopularity';
+export * from './dto/character/characterPropensity';
+export * from './dto/character/characterSetEffect';
+export * from './dto/character/characterSkill';
+export * from './dto/character/characterStat';
+export * from './dto/character/characterSymbolEquipment';
+export * from './dto/character/characterVMatrix';
+
+export * from './dto/guild/guild';
+export * from './dto/guild/guildBasic';
+
+export * from './dto/history/cubeHistory';
+export * from './dto/history/potentialHistory';
+export * from './dto/history/starforceHistory';
+
+export * from './dto/notice/cashshopNoticeDetail';
+export * from './dto/notice/cashshopNoticeList';
+export * from './dto/notice/eventNoticeDetail';
+export * from './dto/notice/eventNoticeList';
+export * from './dto/notice/noticeDetail';
+export * from './dto/notice/noticeList';
+export * from './dto/notice/updateNoticeDetail';
+export * from './dto/notice/updateNoticeList';
+
+export * from './dto/ranking/achievementRanking';
+export * from './dto/ranking/dojangRanking';
+export * from './dto/ranking/guildRanking';
+export * from './dto/ranking/overallRanking';
+export * from './dto/ranking/theSeedRanking';
+export * from './dto/ranking/unionRanking';
+
+export * from './dto/union/union';
+export * from './dto/union/unionArtifact';
+export * from './dto/union/unionChampion';
+export * from './dto/union/unionRaider';
+
+export * from './dto/inspectionInfo';
