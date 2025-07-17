b3e0eff3c7c93c3568929b61fe0e708256212414
SpiralMoon <dev.spiralmoon@gmail.com>
2025-07-17 17:20:01 +0900

    test: (c#) Create achievement api test code

---
+++ csharp/MapleStory.Test/UserAPI.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using MapleStory.OpenAPI.KMS;
+
+namespace MapleStory.Test
+{
+    [TestFixture]
+    public class UserAPI
+    {
+        private static readonly string apiKey = Environment.GetEnvironmentVariable("API_KEY")!; // Your API Key
+        private static readonly MapleStoryAPI api = new MapleStoryAPI(apiKey);
+
+        [Test]
+        public async Task GetAchievement()
+        {
+            var response = await api.GetAchievement();
+            Console.WriteLine(response.ToJson());
+        }
+    }
+}
