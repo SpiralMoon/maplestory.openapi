ab9abfc27b26ef1c8a3b73bf832d134ab726613c
SpiralMoon <dev.spiralmoon@gmail.com>
2025-11-04 21:04:00 +0900

    chore: (c#) Setup appsettings.json config

---
+++ csharp/MapleStory.Test/EnvConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MapleStory.Test
+{
+    public class EnvConfig
+    {
+        private static IConfigurationRoot Configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Local.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        public static string Get(string key)
+        {
+            return Configuration[key] ?? throw new InvalidOperationException($"{key} not found.");
+        }
+    }
+}
